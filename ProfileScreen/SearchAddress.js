996b6dfa6154e6f694c4df1bbbf714a7b3d7f279
hellokack <hi@donga.ac.kr>
2025-06-02 17:55:41 +0900

    주소입력창 SafeArea적용

--- ProfileScreen/SearchAddress.js
+++ ProfileScreen/SearchAddress.js
@@ -1,7 +1,8 @@
-//주소찾기
+// 주소찾기
 import React from 'react';
-import { View, Alert } from 'react-native';
+import { View, Alert, StyleSheet } from 'react-native';
 import Postcode from '@thewoowon/react-native-daum-postcode';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function SearchAddressScreen({ navigation }) {
   const handleAddressSelect = (data) => {
@@ -21,13 +22,22 @@ export default function SearchAddressScreen({ navigation }) {
   };
 
   return (
-    <View style={{ flex: 1 }}>
-      <Postcode
-        style={{ width: '100%', height: '100%' }}
-        jsOptions={{ animation: true }}
-        onSelected={handleAddressSelect}
-        onError={(error) => Alert.alert('주소 검색 오류', JSON.stringify(error))}
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{ flex: 1 }}>
+        <Postcode
+          style={{ width: '100%', height: '100%' }}
+          jsOptions={{ animation: true }}
+          onSelected={handleAddressSelect}
+          onError={(error) => Alert.alert('주소 검색 오류', JSON.stringify(error))}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FBFAFB',
+  },
+});
